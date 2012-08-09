bcac459b19e2cebc4213ab0a06467791b8f446d3
Artem Andreev <andreev.artem@gmail.com>
2012-08-09 17:41:50 +0400

    Working implementation through _datetime: '=ngModel'

--- ui-datetime/ui-datetime.js
+++ ui-datetime/ui-datetime.js
@@ -4,25 +4,27 @@ angular.module("ExperimentsModule", [])
             restrict: 'EA',
             replace: true,
             template: '<div class="ui-datetime">' +
-                        '<input type="text" ng-model="_date" class="date">' +
-                        '<input type="number" ng-model="_hours" min="0" max="23" class="hours">' +
-                        '<input type="number" ng-model="_minutes" min="0" max="59" class="minutes">' +
-                      '</div>',
+                '<input type="text" ng-model="_date" class="date">' +
+                '<input type="number" ng-model="_hours" min="0" max="23" class="hours">' +
+                '<input type="number" ng-model="_minutes" min="0" max="59" class="minutes">' +
+                '</div>',
             require: 'ngModel',
-            scope: true,
-            link: function(scope, element, attrs, ngModelCtrl) {
+            scope: {
+                _datetime: '=ngModel'
+            },
+            link: function(scope, element, attrs) {
                 var elDate = element.find('input.date');
 
-                ngModelCtrl.$render = function () {
-                    var date = new Date(ngModelCtrl.$viewValue);
+                scope.$watch('_datetime', function (newValue, oldValue) {
+                    var date = new Date(newValue);
                     var fillNull = function(num) {
                         if (num < 10) return '0' + num;
                         return num;
                     };
-                    scope._date = fillNull(date.getDate() + 1) + '.' + fillNull(date.getMonth() + 1) + '.' + date.getFullYear();
+                    scope._date = fillNull(date.getDate()) + '.' + fillNull(date.getMonth() + 1) + '.' + date.getFullYear();
                     scope._hours = date.getHours();
                     scope._minutes = date.getMinutes();
-                };
+                }, true);
 
                 elDate.datepicker({
                     dateFormat: 'dd.mm.yy',
@@ -35,10 +37,9 @@ angular.module("ExperimentsModule", [])
                 var watchExpr = function() {
                     var res = scope._date.split('.');
                     if (res.length == 3) return new Date(res[2], res[1] - 1, res[0], scope._hours, scope._minutes);
-                    return 0;
                 };
-                scope.$watch(watchExpr, function (newValue) {
-                    ngModelCtrl.$setViewValue(newValue);
+                scope.$watch(watchExpr, function (newValue, oldValue) {
+                    scope._datetime = newValue;
                 }, true);
             }
         };
